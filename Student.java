5429d3409ede7c3e1461d2aaefa354dcf47fe16b
vm21484n <vikasmatcha41@gmail.com>
2017-03-02 14:13:40 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,5 +1,46 @@
 public class Student {
 
-//
-
+private String id;
+private ArrayList<String> Queries = new ArrayList<String>();
+private double gpa;
+private int totalQueries;
+private int totalschoolRelated;
+private int totalnonSchoolRelated;
+private double  percentSR;
+  
+public student()
+{
+  
+}
+  
+public String getID()
+{
+  return id;
+}
+public double getGpa()
+{ 
+  return gpa;
+}
+  
+public int getTotalQueries()
+{
+  return totalQueries;
+}
+ 
+public int getTotalschoolRelated()
+{
+   return totalschoolRelated;
+}
+ 
+public int gettotalnonSchoolRelated()
+{
+    return totalnonSchoolRelated;
+}
+  
+public double getPercentSR()
+{
+  return percentSR();
+  
+}  
+    
 }
