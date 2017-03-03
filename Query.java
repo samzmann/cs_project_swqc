cd2c402934e922f4b7db2034cf491269a981083c
vm21484n <vikasmatcha41@gmail.com>
2017-03-03 12:20:25 -0500

    Update Query.java

--- Query.java
+++ Query.java
@@ -1,5 +1,35 @@
 public class Query {
 
-//
+ private int Hours;  
+ private int Minutes;
+ private int Day;
+ private int Month;
+ private int Year;
+ 
+Time()
+{
+}
+public int getHours()
+{
+  return Hours;
+}
+public int getMinuntes()
+{
+  return Minutes;
+}
+
+ pubilc int getDay()
+ {
+   return Day;
+ }
+public int getMonth()
+{
+  return Month;
+}
+
+public int getYear()
+{
+  return Year;
+}
 
 }
