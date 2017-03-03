e4cdd99069e1934008b56d7d7ab70e246312066f
vm21484n <vikasmatcha41@gmail.com>
2017-03-03 11:54:27 -0500

    Update Time.java

--- Time.java
+++ Time.java
@@ -1,5 +1,20 @@
 public class Time {
 
-//
+ private double queryid;
+ private String user;
+ private String term;
+ private String domain;
+ private boolean schoolRelated;
+ private int count;
+ 
+Query()
+{
+}
+
+public double getQid()
+{
+  this.
+}
+  
 
 }
