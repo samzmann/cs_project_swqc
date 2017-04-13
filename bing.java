2624ce2290ed79bee05d1aa017f20b9dfaf04d68
vm21484n <vikasmatcha41@gmail.com>
2017-04-13 15:18:36 -0400

    Update bing.java

--- bing.java
+++ bing.java
@@ -30,22 +30,21 @@ public class Bing {
 		this.offset = offset;
 		this.limit = limit;
 	}
+
+	 
 	
 	public void searchResults() throws FileNotFoundException {
 		
-			
 			
 		    PrintStream out = new PrintStream(new File("enrichedQueries.txt"));
 		    ArrayList<String> queries = new ArrayList<String>();
 		    int count=1;
 		   try{
-			   Connection con = getConnection();
-			   PreparedStatement statement  = con.prepareStatement("Select Column1 from query;");
-			   ResultSet result1  = statement.executeQuery();
-			    while(result1.next())
-			    	{
-			    	 queries.add(result1.getString("Column1"));
-			    	}
+			  // Connection con = getConnection();
+			
+			   String statement1="Select Column1 from query where status='0';";
+			   queries=get(statement1,"Column1");
+			 
 			  System.out.println(queries);
 			   
 			
@@ -70,7 +69,6 @@ public class Bing {
 					JSONObject result = resultArray.getJSONObject(i);
 					resultName.add(result.getString("name"));
 					resultSnippet.add(result.getString("snippet"));
-					System.out.print(count1+",");
 					count1++;
 				}
 			} catch (JSONException e) {
@@ -84,32 +82,22 @@ public class Bing {
 			{
 				out.println(resultName.get(i));
 			    out.println(resultSnippet.get(i));
-				
-			    String query1 ="UPDATE query SET enriched=concat(ifnull(enriched,' '),' ',?,' ',?) where Column1=?";
-				PreparedStatement posted=con.prepareStatement(query1);
-				posted.setString(1,resultName.get(i)+"\n");
-				posted.setString(2,resultSnippet.get(i)+"\n");
-				posted.setString(3,query);
-				posted.executeUpdate();
-				
-			}
 			
+			    String statement ="UPDATE query SET enriched=concat(ifnull(enriched,' '),' ',?) where Column1=?";
+				post(statement,resultName.get(i)+" "+resultSnippet.get(i)+" ",query);
+			}
 			
 			out.println();
 			count++;
-			if( count1>=4)
-				{
-				String query2="update query set status=1 where Column1=?";
-				PreparedStatement posted1=con.prepareStatement(query2);
-				posted1.setString(1,query);
-				posted1.executeUpdate();
+			if( count1>1)
+				{						
+				 String statement="update query set status=? where Column1=?";
+				 post(statement,"1",query);
 				}
 			else
-			{
-				String query2="update query set enriched=null ,set status=0 where Column1=?";
-				PreparedStatement posted1=con.prepareStatement(query2);
-				posted1.setString(1, query);
-				posted1.executeUpdate();
+			{  
+				String statement="update query set enriched=null ,status=? where Column1=?";
+				post(statement,"0",query);
 				
 			}
 		}
@@ -121,26 +109,12 @@ public class Bing {
 		else 
 			System.out.println("\nEmpty File.\n");
 		}
-		catch(Exception e){System.out.println(e);}
+		catch(Exception e){
+			System.out.println(e);
+			}
 	}
 	
-	public static Connection getConnection() throws Exception{
-		try{
-		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://localhost:3306/student";// connecting to a database "student"
-		String username = "root";
-		String password = "Mba@2016";
-		Class.forName(driver); 
-		Connection conn = DriverManager.getConnection(url+"?useSSL=false",username,password);
-		System.out.println("Connected");
-		return conn;
-		} catch(Exception e){System.out.println(e);
-		} 
-		return null;
-		}
 	
-	
-
 	private String search(String keyString, int offset, int limit) {
 		HttpClient httpclient = HttpClients.createDefault();
 		try {
@@ -167,4 +141,50 @@ public class Bing {
 		}
 		return "{}";
 	}
+
+public static Connection getConnection() throws Exception{
+	try{
+	String driver = "com.mysql.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/student";// connecting to a database "student"
+	String username = "root";
+	String password = "Mba@2016";
+	Class.forName(driver); 
+	Connection conn = DriverManager.getConnection(url+"?useSSL=false",username,password);
+	//System.out.println("Connected");
+	return conn;
+	} catch(Exception e){System.out.println(e);
+	} 
+	return null;
+	}
+
+public static ArrayList<String> get( String query, String Column) throws Exception
+{
+	 try{
+		 
+		 Connection con=getConnection();
+		 PreparedStatement statement= con.prepareStatement(query);
+		 ResultSet result=statement.executeQuery();
+		 ArrayList<String> array= new ArrayList<String>();
+		 while(result.next())
+	    	{
+	    	 array.add(result.getString(Column));
+	    	}
+		  return array;
+		 
+	 }
+	 catch(Exception e){System.out.println(e);
+	 }
+return null;
+}
+public static void post(String statement,String a1,String a2) throws Exception{
+	 try{
+		 Connection con=getConnection();
+		 PreparedStatement posted=con.prepareStatement(statement);
+			posted.setString(1,a1);
+			posted.setString(2,a2);			
+			posted.executeUpdate();
+		 }catch(Exception e){System.out.println(e);}
+	      finally { 
+		 		}
+}
 }
