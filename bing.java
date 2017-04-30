54be9b95dcd9d9599479638a5d26cb4d161160c9
vm21484n <vikasmatcha41@gmail.com>
2017-04-29 20:09:10 -0400

    Update bing.java

--- bing.java
+++ bing.java
@@ -2,13 +2,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.net.URI;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
 import java.util.ArrayList;
 //import java.util.Scanner;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -20,7 +19,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Bing {
+public class Bing extends db {
 	private String apiKey;
 	private int offset;
 	private int limit;
@@ -31,8 +30,7 @@ public class Bing {
 		this.limit = limit;
 	}
 
-	 
-	
+		
 	public void searchResults() throws FileNotFoundException {
 		
 			
@@ -40,11 +38,11 @@ public class Bing {
 		    ArrayList<String> queries = new ArrayList<String>();
 		    int count=1;
 		   try{
-			  		
-			   String statement1="Select Column1 from query where status='0';";// selects all queries which are not enriched( status is 0)
-			   queries=get(statement1,"Column1");
+			  			  
+			  // queries=get("Select query from swqc1 where status='0';","query");
+			   queries=get("Select query from random;","query");
 			 
-			  System.out.println(queries);
+			  System.out.println(queries.size());
 			   
 			
 			  if(queries.size()>0){
@@ -53,13 +51,14 @@ public class Bing {
 		    	   {
 			
 			String query = queries.get(count-1);
-			out.println("\n"+count);
+			
 			System.out.println(count + ". " + query);
 			
 			String results = search(query, offset, limit);
+			
 			ArrayList<String> resultName = new ArrayList<>();
 			ArrayList<String> resultSnippet = new ArrayList<>();      
-			int count1=1;
+			
 			try {
 				JSONObject searchResponseJson = new JSONObject(results);
 				JSONArray resultArray = searchResponseJson.getJSONObject("webPages").getJSONArray("value");
@@ -68,7 +67,7 @@ public class Bing {
 					JSONObject result = resultArray.getJSONObject(i);
 					resultName.add(result.getString("name"));
 					resultSnippet.add(result.getString("snippet"));
-					count1++;// increments number of results are add added for enrichment
+					
 				}
 			} catch (JSONException e) {
 				//Debug.println("Error in Json reading");
@@ -77,28 +76,21 @@ public class Bing {
 							
 			}
 			
+			String enriched="";
+			
 			for (int i = 0; i < resultName.size(); i++)
-			{
+			{   
 				out.println(resultName.get(i));
 			    out.println(resultSnippet.get(i));
-			
-			    String statement ="UPDATE query SET enriched=concat(ifnull(enriched,' '),' ',?) where Column1=?";
-				post(statement,resultName.get(i)+" "+resultSnippet.get(i)+" ",query);//updates enriched column
-			}
-			
-			out.println();
-			count++;
-			if( count1>1)// when there is loss of internet connection or some problem count1=1,to make query enrich again we use this condition to change the status from 0 to 1
-				{						
-				 String statement="update query set status=? where Column1=?";
-				 post(statement,"1",query); // if the query is enriched completely ,status is 1
-				}
-			else
-			{  
-				String statement="update query set enriched=null ,status=? where Column1=?";
-				post(statement,"0",query);// if query is not enriched status is 0//                    
 				
-			}
+				enriched=enriched+resultName.get(i)+"\n "+ resultSnippet.get(i)+ "\n ";
+			}  
+			   			    
+			   String statement ="update random set enriched=concat(ifnull(enriched,' '),?) where query=?";  
+			   update(statement,enriched,query);
+			    
+			count++;
+			
 		}
 			    	
 		
@@ -109,7 +101,8 @@ public class Bing {
 			System.out.println("\nEmpty File.\n");
 		}
 		catch(Exception e){
-			System.out.println(e);
+			System.out.println();
+			
 			}
 	}
 	
@@ -140,5 +133,4 @@ public class Bing {
 		}
 		return "{}";
 	}
-
 }
