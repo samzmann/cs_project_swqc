5c3dab456c60f84290fe2a8f661113436c8cd41b
wolf1991 <worfxu@gmail.com>
2017-04-29 20:11:46 -0400

    111

--- src/team11/jdbctest.java
+++ src/team11/jdbctest.java
@@ -18,9 +18,9 @@ public class jdbctest {
     	Connection conn = null;
         try {
         	conn = DBUtil.getConnection();
-//        	jdbctest.migrateDataFromCSVtoDB(conn);
+        	// jdbctest.migrateDataFromCSVtoDB(conn);
 //        	jdbctest.randomPickStudents();
-        	jdbctest.getSpecificStudentsQuery(conn);
+//        	jdbctest.getSpecificStudentsQuery(conn);
 
         } catch (Exception e) {
         	e.printStackTrace();
@@ -31,34 +31,37 @@ public class jdbctest {
     
     public static void migrateDataFromCSVtoDB(Connection conn) throws Exception {
 		String[] header = {};
-
-		ArrayList<String> uid = new ArrayList<String>();
-		ArrayList<String> data = new ArrayList<String>();
-		ArrayList<String> domain = new ArrayList<String>();
-		ArrayList<String> timestamp = new ArrayList<String>();
-		ArrayList<String> count = new ArrayList<String>();
-    	CsvReader reader = new CsvReader(
-				"/Users/Tim/Desktop/research paper/CapstoneCourseSpring2017/data/finaldatasetpart2(Tim)_onlyone.csv");
-    	while (reader.readRecord()) {
-			// save header of cvs
-			if (reader.getCurrentRecord() == 0) {
-				System.out.println("Its 0 row");
-				header = reader.getValues();
-			} else {
-				uid.add(reader.getValues()[0]);
-				data.add(reader.getValues()[1]);
-				domain.add(reader.getValues()[2]);
-				timestamp.add(reader.getValues()[3]);
-				count.add(reader.getValues()[4]);
-				try {
-					DBUtil.post(conn, "INSERT INTO `cs_project_swqc`.`dataset` (`studentID`, `searchQuery`, `domain`, `time`, `count`) VALUES (?, ?, ?, ?, ?);", reader.getValues());
-				} catch (SQLException e) {
-		            e.printStackTrace();
-		        }
-				
+		CsvReader reader = null;
+		try {
+			ArrayList<String> uid = new ArrayList<String>();
+			ArrayList<String> data = new ArrayList<String>();
+			ArrayList<String> domain = new ArrayList<String>();
+			ArrayList<String> timestamp = new ArrayList<String>();
+			ArrayList<String> count = new ArrayList<String>();
+	    	reader = new CsvReader(
+					"/Users/Tim/Desktop/research paper/CapstoneCourseSpring2017/data/finaldatasetpart2(Tim)_onlyone.csv");
+	    	while (reader.readRecord()) {
+				// save header of cvs
+				if (reader.getCurrentRecord() == 0) {
+					System.out.println("Its 0 row");
+					header = reader.getValues();
+				} else {
+					uid.add(reader.getValues()[0]);
+					data.add(reader.getValues()[1]);
+					domain.add(reader.getValues()[2]);
+					timestamp.add(reader.getValues()[3]);
+					count.add(reader.getValues()[4]);
+					
+					DBUtil.post(conn, "INSERT INTO `cs_project_swqc`.`dataset` (`student_id`, `search_query`, `domain`, `time`, `count`) VALUES (?, ?, ?, ?, ?);", reader.getValues());
+				}
 			}
-		}
-		reader.close();
+		} catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        	DBUtil.closeConnection(conn);
+			reader.close();
+        }
+    	
     }
 
     public static void randomPickStudents() {
